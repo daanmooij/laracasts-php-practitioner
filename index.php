9444b51b0e558e4278700c83095867e78fc6d822
Daan Mooij <daanmooij@gmail.com>
2018-06-23 14:56:00 +0200

    use functions

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
+require 'functions.php';
+
 $task = [
 	'title' => 'Finish homework',
-	'due' => 'today',
-	'assigned_to' => 'Daan',
-	'completed' => true
+    'due' => 'today',
+    'assigned_to' => 'Daan',
+    'completed' => true
 ];
 
 require 'index.view.php';

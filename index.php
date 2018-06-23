b43c0939b1ceb61871fc5b75172d51f95c3e0a30
Daan Mooij <daanmooij@gmail.com>
2018-06-23 16:16:20 +0200

    use a Class

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php
 
-require 'functions.php';
+class Task {
 
-$task = [
-	'title' => 'Finish homework',
-    'due' => 'today',
-    'assigned_to' => 'Daan',
-    'completed' => true
+	public $description;
+
+	public $completed = false;
+
+	public function __construct($description)
+	{
+		$this->description = $description;
+	}
+
+	public function complete()
+	{
+		$this->completed = true;
+	}
+
+	public function isComplete(): bool
+	{
+		return $this->completed;
+	}
+}
+
+$tasks = [
+	new Task('Go to the store'),
+	new Task('Finish my screencast'),
+	new Task('Clean my room')
 ];
+
+$tasks[0]->complete();
 
 require 'index.view.php';

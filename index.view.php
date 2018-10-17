837a02b058943afa724323da5520ce2c174edceb
Daan Mooij <daanmooij@gmail.com>
2018-10-17 19:25:35 +0200

    add styling with bootstrap

--- index.view.php
+++ index.view.php
@@ -2,30 +2,28 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
-	<style>
-		header {
-			background: #e3e3e3;
-			padding: 2em;
-			text-align: center;
-		}
-	</style>
+	<title>PHP Practitioner</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:600" rel="stylesheet">
+	<link href="style.css" rel="stylesheet">
 </head>
 <body>
-	<ul>
-		<?php foreach ($tasks as $task) : ?>
-			<li>
-				<?php if ($task->completed) : ?>
-				
-					<strike><?= $task->description; ?></strike>
+	<div class="container text-center">
+		<ul class="list-group">
+			<?php foreach ($tasks as $task) : ?>
+				<li class="list-group-item">
+					<?php if ($task->completed) : ?>
+					
+						<strike><?= $task->description; ?></strike>
 
-				<?php else: ?>
+					<?php else: ?>
 
-					<?= $task->description; ?>
+						<?= $task->description; ?>
 
-				<?php endif; ?>
-			</li>
-		<?php endforeach; ?>
-	</ul>
+					<?php endif; ?>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
 </body>
 </html>

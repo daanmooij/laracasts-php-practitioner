f59fea82f49fd37bc684abee32454084a96e390c
Daan Mooij <daanmooij@gmail.com>
2018-10-17 20:38:08 +0200

    add controllers and routing

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-$database = require 'bootstrap.php';
+require 'core/bootstrap.php';
 
-$tasks = $database->selectAll('todos');
 
-require 'index.view.php';
+
+require Router::load('routes.php')->direct(Request::uri());

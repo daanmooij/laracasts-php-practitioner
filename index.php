0955a0bc45df31935a77f8e0089021444889eb81
Daan Mooij <daanmooij@gmail.com>
2018-10-19 20:12:05 +0200

    restructure router to handle get and post

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require 'core/bootstrap.php';
 
 
 
-require Router::load('routes.php')->direct(Request::uri());
+require Router::load('routes.php')->direct(Request::uri(), Request::method());
--- views/index.view.php
+++ views/index.view.php
@@ -1,21 +1,10 @@
 <?php require('partials/head.php'); ?>
 	
-	<div class="container text-center">
-		<h2>My Tasks</h2>
-		<ul class="list-group">
-			<?php foreach ($tasks as $task) : ?>
-				<li class="list-group-item">
-					<?php if ($task->completed) : ?>
-					
-						<strike><?= $task->description; ?></strike>
+<h3>Submit Your Name</h3>
 
-					<?php else: ?>
+<form method="POST" action="/names">
+	<input name="name"></input>
+	<button type="submit">Submit</button>
+</form>
 
-						<?= $task->description; ?>
-
-					<?php endif; ?>
-				</li>
-			<?php endforeach; ?>
-		</ul>
-	</div>
 <?php require('partials/footer.php'); ?>

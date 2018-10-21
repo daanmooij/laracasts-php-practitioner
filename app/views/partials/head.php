8e759a293cc94820da81577a0a804bda0fcf8d29
Daan Mooij <daanmooij@gmail.com>
2018-10-21 17:06:37 +0200

    use namespaces

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -5,7 +5,7 @@
 	<title>PHP Practitioner</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Nunito:600" rel="stylesheet">
-	<link href="/style.css" rel="stylesheet">
+	<link href="public/css/style.css" rel="stylesheet">
 </head>
 <body>
 
